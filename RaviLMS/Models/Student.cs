257a091d6050d7e7d4da3bd3baa34f5d816b6993
Saiganesh2202 <saiganeshraja@gmail.com>
2025-05-29 15:06:35 +0530

    validation

--- RaviLMS/Models/Student.cs
+++ RaviLMS/Models/Student.cs
@@ -8,10 +8,11 @@ namespace RaviLMS.Models
 
         [Required(ErrorMessage = "Full Name is required")]
         [StringLength(100, ErrorMessage = "Full Name cannot exceed 100 characters")]
+        [RegularExpression(@"^[^\d]*$", ErrorMessage = "Full Name cannot contain numbers")]
         public string FullName { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessages = "Invalid Email Address")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
